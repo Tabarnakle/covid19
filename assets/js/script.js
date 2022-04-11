776529d8e9202079cff80cc8181724b9c5aefc4c
Benja <abarzuabenjamin@gmail.com>
2022-04-11 19:25:32 -0400

    Comenta funcion innecesaria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-// Crear un evento que al hacer click en "Mostrar Grafico", nos rescate la informacion de la API
+/* // Crear un evento que al hacer click en "Mostrar Grafico", nos rescate la informacion de la API
 $("#btn-show").submit(async (event) => {
-    event.preventDefault();
-    const submt = getData();
-    toggleHide("btn-hide");
-    //   console.log("click en fommulario");
-  });
-  
-  // Obtenemos la info de la API
-  const getData = async () => {
-    fetch("http://localhost:3000/api/total")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-      });
-  };
+  event.preventDefault();
+  const submt = getData();
+  toggleHide("btn-hide");
+  //   console.log("click en fommulario");
+});
+ */
+// Obtenemos la info de la API
+const getData = async () => {
+  fetch("http://localhost:3000/api/total")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
+};
