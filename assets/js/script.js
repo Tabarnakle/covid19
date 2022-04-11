0cd0bc5c8134626c1c807c7ab050c6a9cb251405
Benja <abarzuabenjamin@gmail.com>
2022-04-11 19:29:38 -0400

    Crea funcion createTable

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,22 @@ const getData = async () => {
       console.log(json);
     });
 };
+// crea la tabla a partir de getData
+const createTable = async () => {
+  const table = document.getElementById("covidTable");
+  const data = await getData();
+  data.forEach((country) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `<th scope="row">${
+      country.location
+    }</th><td>${country.confirmed.toLocaleString()}</td><td>${country.deaths.toLocaleString()}</td><td>${
+      country.recovered
+    }</td><td>${
+      country.active
+    }</td><td><button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#countryModal" data-bs-country="${
+      country.location
+    }">Ver detalles</button></td>`;
+    table.appendChild(row);
+  });
+};
+createTable();
