bc19b85e94acb5b3cad0bc871f46e1f93b1a9cc5
Benja <abarzuabenjamin@gmail.com>
2022-04-11 20:36:37 -0400

    Agrega modal detalles al presionar boton

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ const createTable = async () => {
       country.recovered
     }</td><td>${
       country.active
-    }</td><td><button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#countryModal" data-bs-country="${
+    }</td><td><button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#countryModal" data-country="${
       country.location
     }">Ver detalles</button></td>`;
     table.appendChild(row);
@@ -31,3 +31,39 @@ const createTable = async () => {
   table.classList.remove("d-none");
 };
 createTable();
+
+// obtiene la informacion del endpoint /countries/$country
+const getDataCountry = async (country) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/countries/${country}`
+    );
+    const { data } = await response.json();
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    localStorage.clear();
+    console.error(`Error: ${error}`);
+  }
+};
+
+// Muestra el modal detallando la info de cada pais y lo actualiza con la info de getDataCountry
+$("#countryModal").on("show.bs.modal", (event) => {
+  const button = event.relatedTarget;
+  const country = button.getAttribute("data-country");
+
+  const countryData = async () => {
+    const modalTitle = countryModal.querySelector(".modal-title");
+    const modalBody = countryModal.querySelector(".modal-body");
+    const data = await getDataCountry(country);
+    modalTitle.textContent = "Situacion COVID en " + data.location;
+    modalBody.innerHTML = `<ul>
+    <li>Casos confirmados: ${data.confirmed.toLocaleString()}</li>
+    <li>Muertes: ${data.deaths.toLocaleString()}</li>
+    <li>Casos activos: ${data.active.toLocaleString()}</li>
+    <li>Recuperados: ${data.recovered.toLocaleString()}</li>
+    </ul>`;
+  };
+  countryData();
+});
